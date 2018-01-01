9daf187fe7c0eb8b88053d4417ada0d72623d818
adg3748 <revivedtomorrow@gmail.com>
2018-01-01 23:32:03 +0900

    display news' titles

--- App.js
+++ App.js
@@ -6,28 +6,40 @@ import {
   FlatList,
   Button
 } from 'react-native';
-import ReactDOM from 'react-dom';
 
 export default class App extends Component {
 
   constructor(props){
     super(props);
-    this.state = { movies: [] };
-    function sample() {
+    this.state = { threads: '' };
+    function _fetch(_this) {
       fetch('https://www.reddit.com/r/newsokur/hot.json') // 返値はPromise型。resolveならthen、rejectならcatchを実行する
         .then((response) => response.json())
         .then((responseJson) => {
-          console.log(responseJson);
+          let threads = responseJson.data.children; // 配列の中にオブジェクトがたくさん
+          threads = threads.map( i => {
+            i.key = i.data.url // keyがないと警告が出る
+            console.log(i.data.url);
+            return i
+          });
+          _this.setState({threads: threads});
         })
         .catch((error) => {
-          console.error(error);
         });
     }
-    sample();
+    _fetch(this);
   }
   render() {
+    console.log(this.state.threads.length);
     return (
       <View style={styles.container}>
+        <FlatList
+          data={this.state.threads}
+          extraData={this.state.threads}
+          renderItem={(thread) => // アロー関数っぽく見えるが、ロケットの右側に{}を書いてしまうと表示できない
+            <Text>{thread.item.data.title}</Text>
+          }
+        />
       </View>
     );
   }
@@ -35,10 +47,6 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#f5fcff',
   },
 })
 
