3058580efc8d15c624c8aa77152fc9b5733b0e70
adg3748 <revivedtomorrow@gmail.com>
2018-01-01 21:30:40 +0900

    fetch reddit and put on console

--- App.js
+++ App.js
@@ -13,62 +13,21 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = { movies: [] };
-    this.getMoviesFromApiAsync = this.getMoviesFromApiAsync.bind(this); // イベントハンドラの設定をする場合、bindしないとthisの内容が変わってしまい、setStateがundefiedだとエラーが出る
-     this._console = this._console.bind(this);
+    function sample() {
+      fetch('https://www.reddit.com/r/newsokur/hot.json') // 返値はPromise型。resolveならthen、rejectならcatchを実行する
+        .then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     }
-  getMoviesFromApiAsync() { // 文頭にfunctionがついてないのは、関数ではなくイベントハンドラの定義であるため
-    return fetch('https://facebook.github.io/react-native/movies.json')
-      /*
-        {
-          "title": "The Basics - Networking",
-          "description": "Your app fetched this from a remote endpoint!",
-          "movies": [
-            { "title": "Star Wars", "releaseYear": "1977"},
-            { "title": "Back to the Future", "releaseYear": "1985"},
-            { "title": "The Matrix", "releaseYear": "1999"},
-            { "title": "Inception", "releaseYear": "2010"},
-            { "title": "Interstellar", "releaseYear": "2014"}
-          ]
-        }
-      */
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({movies :responseJson.movies});
-        /*
-          [exp] Array [
-          [exp]   Object {
-          [exp]     "releaseYear": "1977",
-          [exp]     "title": "Star Wars",
-          [exp]   },
-          [exp]   Object {
-          [exp]     "releaseYear": "1985",
-          [exp]     "title": "Back to the Future",
-          [exp]   },
-          [exp]   Object {
-          [exp]     "releaseYear": "1999",
-          [exp]     "title": "The Matrix",
-          [exp]   },
-          [exp]   Object {
-          [exp]     "releaseYear": "2010",
-          [exp]     "title": "Inception",
-          [exp]   },
-          [exp]   Object {
-          [exp]     "releaseYear": "2014",
-          [exp]     "title": "Interstellar",
-          [exp]   },
-          [exp] ]
-          */
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    sample();
   }
-  _console() { console.log(this.state.movies); }
   render() {
     return (
       <View style={styles.container}>
-        <Button onPress={this.getMoviesFromApiAsync} title="tap me!" />
-        <Button onPress={this._console} title="tap me!" />
       </View>
     );
   }
