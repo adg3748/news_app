407dd6e2450f50f45342cd756042bd73a1ac84fd
adg3748 <revivedtomorrow@gmail.com>
2018-01-01 18:42:59 +0900

    press bottun and put data on console

--- App.js
+++ App.js
@@ -3,65 +3,68 @@ import {
   StyleSheet,
   Text,
   View,
-  FlatList
-} from 'react-native'
+  FlatList,
+  Button
+} from 'react-native';
+import ReactDOM from 'react-dom';
 
 export default class App extends Component {
 
   constructor(props){
-    super(props)
-    function getMoviesFromApiAsync() {
-      return fetch('https://facebook.github.io/react-native/movies.json')
-        /*
-          {
-            "title": "The Basics - Networking",
-            "description": "Your app fetched this from a remote endpoint!",
-            "movies": [
-              { "title": "Star Wars", "releaseYear": "1977"},
-              { "title": "Back to the Future", "releaseYear": "1985"},
-              { "title": "The Matrix", "releaseYear": "1999"},
-              { "title": "Inception", "releaseYear": "2010"},
-              { "title": "Interstellar", "releaseYear": "2014"}
-            ]
-          }
-        */
-        .then((response) => response.json())
-        .then((responseJson) => {
-          console.log(responseJson.movies);
-          /*
-            [exp] Array [
-            [exp]   Object {
-            [exp]     "releaseYear": "1977",
-            [exp]     "title": "Star Wars",
-            [exp]   },
-            [exp]   Object {
-            [exp]     "releaseYear": "1985",
-            [exp]     "title": "Back to the Future",
-            [exp]   },
-            [exp]   Object {
-            [exp]     "releaseYear": "1999",
-            [exp]     "title": "The Matrix",
-            [exp]   },
-            [exp]   Object {
-            [exp]     "releaseYear": "2010",
-            [exp]     "title": "Inception",
-            [exp]   },
-            [exp]   Object {
-            [exp]     "releaseYear": "2014",
-            [exp]     "title": "Interstellar",
-            [exp]   },
-            [exp] ]
-            */
-        })
-        .catch((error) => {
-          console.error(error);
-        });
+    super(props);
+    this.state = { movies: [] };
     }
-  getMoviesFromApiAsync();
+  getMoviesFromApiAsync() {
+    return fetch('https://facebook.github.io/react-native/movies.json')
+      /*
+        {
+          "title": "The Basics - Networking",
+          "description": "Your app fetched this from a remote endpoint!",
+          "movies": [
+            { "title": "Star Wars", "releaseYear": "1977"},
+            { "title": "Back to the Future", "releaseYear": "1985"},
+            { "title": "The Matrix", "releaseYear": "1999"},
+            { "title": "Inception", "releaseYear": "2010"},
+            { "title": "Interstellar", "releaseYear": "2014"}
+          ]
+        }
+      */
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.movies);
+        /*
+          [exp] Array [
+          [exp]   Object {
+          [exp]     "releaseYear": "1977",
+          [exp]     "title": "Star Wars",
+          [exp]   },
+          [exp]   Object {
+          [exp]     "releaseYear": "1985",
+          [exp]     "title": "Back to the Future",
+          [exp]   },
+          [exp]   Object {
+          [exp]     "releaseYear": "1999",
+          [exp]     "title": "The Matrix",
+          [exp]   },
+          [exp]   Object {
+          [exp]     "releaseYear": "2010",
+          [exp]     "title": "Inception",
+          [exp]   },
+          [exp]   Object {
+          [exp]     "releaseYear": "2014",
+          [exp]     "title": "Interstellar",
+          [exp]   },
+          [exp] ]
+          */
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
   render() {
     return (
       <View style={styles.container}>
+        <Button onPress={this.getMoviesFromApiAsync} title="tap me!" />
       </View>
     );
   }
